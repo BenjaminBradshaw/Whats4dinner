9f4ac2de138a9824f266c79ea0bd668bce5ecad0
ben <ben__@live.com.au>
2025-02-27 08:21:09 +1030

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.title("Whats for dinner")
 
 st.subheader('Menu')
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   name = st.text_input("Your name")
+   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
+   st.form_submit_button('Submit my picks')
+
+# This is outside the form
+st.write(my_number)
+st.write(my_color)
