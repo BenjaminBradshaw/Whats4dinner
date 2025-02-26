9be8b625e08e9e689d723da6dd44e56d53f9d3b9
ben <ben__@live.com.au>
2025-02-27 08:27:35 +1030

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ st.subheader('Menu')
 
 with st.form("my_form"):
    st.write("Inside the form")
-   name = st.text_input("Your name")
-   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
+   my_name = st.text_input("Your name")
+   my_main = st.selectbox('Pick a main', ['fish','beef', 'pork','calamari'])
    st.form_submit_button('Submit my picks')
 
 # This is outside the form
-st.write(my_number)
-st.write(my_color)
+st.write(my_main)
+st.write(my_name)
