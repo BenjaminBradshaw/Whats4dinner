20861fbd06d5737f657901800e77bcc0f0341246
ben <ben__@live.com.au>
2025-02-27 08:46:15 +1030

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Whats for dinner")
 st.subheader('Menu')
 
 with st.form("my_form"):
-   st.write("Inside the form")
+   st.write("dinner options")
    my_name = st.text_input("Your name")
    my_starter = st.selectbox("Pick a starter", ["calamari","grilled cabbage"])
    my_main = st.selectbox('Pick a main', ['fish','beef', 'pork','calamari'])
@@ -13,5 +13,9 @@ with st.form("my_form"):
    st.form_submit_button('Submit my picks')
 
 # This is outside the form
-st.write(my_main)
+
 st.write(my_name)
+st.write(my_starter)                           
+st.write(my_main)
+st.write(my_dessert)
+                             
