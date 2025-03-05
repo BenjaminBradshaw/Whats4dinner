aed41d1637b925a3d64649419fd94cb894e68849
ben <ben__@live.com.au>
2025-03-05 19:35:31 +1100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,40 @@
 import streamlit as st
+import pandas as pd
+import pyodbc
+import os
+
+
+def init_connection():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + os.getenv("AZURE_SQL_SERVER")
+        + ";DATABASE="
+        + os.getenv("AZURE_SQL_DATABASE")
+        + ";UID="
+        + os.getenv("AZURE_SQL_USER")
+        + ";PWD="
+        + os.getenv("AZURE_SQL_PASSWORD")
+    )
+
+conn = init_connection()
+
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
 st.header("Settings")
+
+
 st.write(f"You are logged in as {st.session_state.role}.")
+
+
+
+rows = run_query("SELECT * FROM [dbo].[Menu];")
+
+st.write(pd.DataFrame(rows))
+
+edited_df = st.data_editor(rows)
+
+
+
